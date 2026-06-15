be396835cede2aed83d90432da98518c2d915dec
deepakSingh023 <dsinghrana71@gmail.com>
2026-06-15 18:50:26 +0530

    security config update so that actuator works

--- src/main/java/com/example/social_reel/config/SecurityConfig.java
+++ src/main/java/com/example/social_reel/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/health").permitAll()
                         .requestMatchers("/api/reel/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(internalFilter, UsernamePasswordAuthenticationFilter.class)
